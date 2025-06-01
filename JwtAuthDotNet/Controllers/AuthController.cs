db36fc38f71bb647ddc25988ccf3080abcf9810e
obiscode <tobynwude@gmail.com>
2025-06-01 13:14:21 +0100

    Add AuthController login

--- JwtAuthDotNet/Controllers/AuthController.cs
+++ JwtAuthDotNet/Controllers/AuthController.cs
@@ -1,13 +1,16 @@
+using System.Security.Claims;
+using System.Text;
 using JwtAuthDotNet.Data.Dtos;
 using JwtAuthDotNet.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JwtAuthDotNet.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AuthController : ControllerBase
+public class AuthController(IConfiguration configuration) : ControllerBase
 {
     public static User user = new();
     
@@ -23,5 +26,23 @@ public class AuthController : ControllerBase
 
         return Ok(user);
     }
-    
+
+    [HttpPost("login")]
+    public ActionResult<string> Login(UserDto request)
+    {
+            //debugging ppurpose
+            // if (user.Username != request.Username)
+            // {
+            //     return BadRequest("User not found");
+            // }
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password)
+                == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong username / password.");
+            }
+
+            string token = "success";
+            return Ok(token);
+    }
 }
